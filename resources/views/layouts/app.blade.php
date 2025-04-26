bb953fc106699d8ae4b2c9bcc906574efa3e2c66
mariscaldev <mariscaldev@gmx.com>
2025-04-25 23:32:46 -0400

    Pusheo de public/build

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,16 +13,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Fira+Code&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
-    @php
-        function vite_asset($asset)
-        {
-            $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
-            return asset('build/' . $manifest[$asset]['file']);
-        }
-    @endphp
-
-    <link rel="stylesheet" href="{{ vite_asset('resources/sass/app.scss') }}">
-    <script src="{{ vite_asset('resources/js/app.js') }}" defer></script>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
@@ -168,8 +159,7 @@
                         <a href="{{ route('register') }}"><i class="bi bi-person-plus"></i> Register</a>
                     @endif
                 @else
-                    <div class="text-center text-white fw-bold mb-3">Hola, <span
-                            class="text-primary">{{ Auth::user()->name }}</span></div>
+                    <div class="text-center text-white fw-bold mb-3">Hola, <span class="text-primary">{{ Auth::user()->name }}</span></div>
                     <a href="{{ url('/home') }}" class="{{ request()->is('home') ? 'active-link' : '' }} mb-3">
                         <i class="bi bi-house-door"></i> Inicio
                     </a>
