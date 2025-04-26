f71c35e135adb4c6bd7fc4b735eebbcddb8b6d4f
mariscaldev <mariscaldev@gmx.com>
2025-04-26 01:45:58 -0400

    Fix Api.php

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 
-Route::get('/canciones', [ApiController::class, 'canciones']);
-Route::get('/listas', [ApiController::class, 'listas']);
+// ▶ API para la app móvil
+Route::middleware('api')->group(function () {
+    Route::get('/canciones', [ApiController::class, 'canciones']);
+    Route::get('/listas', [ApiController::class, 'listas']);
+});
