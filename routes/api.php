55889000b07c66da1171244403bc685b628f8f1f
mariscaldev <mariscaldev@gmx.com>
2025-04-26 01:19:41 -0400

    Arreglo de apis

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
 
-// ▶ API para la app móvil
 Route::get('/canciones', [ApiController::class, 'canciones']);
 Route::get('/listas', [ApiController::class, 'listas']);
