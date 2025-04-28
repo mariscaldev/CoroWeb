f1cb2cae66fd255b4d0e5e1e10f60ccf97be6b4b
mariscaldev <mariscaldev@gmx.com>
2025-04-28 01:30:46 -0400

    Endpoints cancion y lista especifica

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -2,19 +2,49 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller; // <<<< Agrega esta línea
+use App\Http\Controllers\Controller;
 use App\Models\{Canciones, Etiquetas, Listas};
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    public function canciones() {
+    public function canciones()
+    {
         $canciones = Canciones::all();
         return response()->json($canciones);
     }
-    
-    public function listas() {
+
+    public function cancion($id)
+    {
+        $cancion = Canciones::find($id);
+
+        if (!$cancion) {
+            return response()->json([
+                'message' => 'La canción no fue encontrada.',
+                'status' => 404
+            ], 404);
+        }
+
+        return response()->json($cancion);
+    }
+
+    public function listas()
+    {
         $listas = Listas::with('canciones')->get();
         return response()->json($listas);
-    }    
+    }
+
+    public function lista($id)
+    {
+        $lista = Listas::with('canciones')->find($id);
+
+        if (!$lista) {
+            return response()->json([
+                'message' => 'La lista no fue encontrada.',
+                'status' => 404
+            ], 404);
+        }
+
+        return response()->json($lista);
+    }
 }
